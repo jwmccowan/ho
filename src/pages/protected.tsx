45e486693ac459093a3b3f8c15af321ef9d08789
John McClown <jwmccowan@gmail.com>
2021-11-18 20:53:24 +1100

    Fix build issues with just to get it online

--- src/pages/protected.tsx
+++ src/pages/protected.tsx
@@ -1,11 +1,12 @@
 import { User } from "@supabase/supabase-js";
+import { NextApiRequest } from "next";
 import { supabase } from "../utils/supabase.client";
 
 export default function Protected({ user }: { user: User }) {
   return <div>{JSON.stringify(user)}</div>;
 }
 
-export async function getServerSideProps({ req }) {
+export async function getServerSideProps({ req }: { req: NextApiRequest }) {
   const { user } = await supabase.auth.api.getUserByCookie(req);
   if (!user) {
     return { props: {}, redirect: { destination: "/sign-in" } };
